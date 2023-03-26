6be59b936e065e51577776a10a25f6c19907b865
tkjsung <9490371+tkjsung@users.noreply.github.com>
2023-03-26 16:13:04 -0700

    Manually added script info

--- iCloud-Control-Script/main.swift
+++ iCloud-Control-Script/main.swift
@@ -8,8 +8,14 @@
 import Foundation
 import FinderSync
 
+var script_version = "1.0.2"
 var dir_exist = false
 var Evict = icloud_evict()
+
+// Project Info: Manual Update (could not figure out how to do automatic using info.plist)
+print("\n############\niCloud Control Script")
+print("Version: \(script_version)\nDate: 2023-03-26")
+print("Copyright (c) 2023 by Tom Sung.\n############")
 
 // If filepath does not exist, get user input again.
 while dir_exist == false{
