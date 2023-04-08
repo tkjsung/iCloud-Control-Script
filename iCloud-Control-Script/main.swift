11e35bfa2e36ff359215ee2410911308f6e0e31f
tkjsung <9490371+tkjsung@users.noreply.github.com>
2023-04-07 22:06:28 -0700

    Added terminal file path input

--- iCloud-Control-Script/main.swift
+++ iCloud-Control-Script/main.swift
@@ -8,22 +8,40 @@
 import Foundation
 import FinderSync
 
-var script_version = "1.0.2"
+// Script Metadata
+var script_version = "1.1.0"
+
+// Necessary variables
 var dir_exist = false
 var Evict = icloud_evict()
+let cmd_line_args = CommandLine.arguments  // First argument always the executable name (at index 0).
 
 // Project Info: Manual Update (could not figure out how to do automatic using info.plist)
 print("\n############\niCloud Control Script")
-print("Version: \(script_version)\nDate: 2023-03-26")
+print("Version: \(script_version)\nDate: 2023-04-07")
 print("Copyright (c) 2023 by Tom Sung.\n############")
 
-// If filepath does not exist, get user input again.
-while dir_exist == false{
-    Evict.get_input()
+
+// Check if command line has additional filepath arguments
+if cmd_line_args.count > 1{
+    Evict.dir = cmd_line_args[1]
+    print("Entered filepath from command line: \"\(Evict.dir)\"\n")
     dir_exist = Evict.verify_directory()
+    if dir_exist == false{
+//        print("The filepath argument is invalid. Try again with a valid filepath.")
+        exit(0)
+    }
+    else{
+        NSLog("The entered directory exists. Attempting file eviction from local drive.")
+    }
+}
+else{
+    while dir_exist == false{
+        Evict.get_input()
+        dir_exist = Evict.verify_directory()
+    }
 }
 
 Evict.get_all_urls()
 Evict.evict_urls()
-
 exit(0)
